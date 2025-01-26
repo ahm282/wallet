f1bd398a9d85002f288640b18dee4fdac2af2244
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-01-27 00:30:26 +0100

    refactor(user-service): enable tests in docker build phase

--- user-service/src/main/java/com/fact/user_service/service/UserService.java
+++ user-service/src/main/java/com/fact/user_service/service/UserService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.swing.text.html.Option;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
