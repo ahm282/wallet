c22106e0e92240b6f69d7277fac88b0d3eba0e9e
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-01-27 20:42:36 +0200

    feat(frontend): add base login form and base logic

--- frontend/wallet/src/app/app.component.ts
+++ frontend/wallet/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, LoginComponent],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wallet';
+    title = 'wallet';
 }
