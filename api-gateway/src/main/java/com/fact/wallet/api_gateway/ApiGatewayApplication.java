d38b66451959b8c84c2a15250690291625aed675
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-09 06:20:10 +0100

    refactor(gateway): Api gateway functional

--- api-gateway/src/main/java/com/fact/wallet/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/fact/wallet/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,12 @@ package com.fact.wallet.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
 }
