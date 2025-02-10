1ef3393feefced38ce992822f50f2e8c7cc2df51
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-10 11:03:40 +0100

    feat(frontend): fix components and layout improvements

--- frontend/src/components/ui/custom-sidebar-trigger.tsx
+++ frontend/src/components/ui/custom-sidebar-trigger.tsx
@@ -1,7 +1,14 @@
 import { useSidebar } from "@/components/ui/sidebar";
+import { PanelsLeftBottom } from "lucide-react";
 
 export function CustomTrigger() {
     const { toggleSidebar } = useSidebar();
 
-    return <button onClick={toggleSidebar}>icon</button>;
+    return (
+        <button
+            className='p-3 rounded-md group hover:bg-gray-100 dark:hover:bg-sidebar-accent'
+            onClick={toggleSidebar}>
+            <PanelsLeftBottom className='size-5 text-muted-foreground' />
+        </button>
+    );
 }
