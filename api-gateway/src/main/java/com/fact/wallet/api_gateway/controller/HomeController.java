0e0225e87dc29701ab83420d1baa79cf26ba7491
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-12 05:51:42 +0100

    refactor(api-gateway): add linting with spotless

--- api-gateway/src/main/java/com/fact/wallet/api_gateway/controller/HomeController.java
+++ api-gateway/src/main/java/com/fact/wallet/api_gateway/controller/HomeController.java
@@ -1,14 +1,21 @@
+/** This package contains controllers for the API Gateway. */
 package com.fact.wallet.api_gateway.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/** Controller for handling home page requests. */
 @RestController
 @RequestMapping("/")
-public class HomeController {
-    @GetMapping
-    public String home() {
-        return "Wallet's API Gateway is operational! 🚀";
-    }
+public final class HomeController {
+  /**
+   * Returns a welcome message.
+   *
+   * @return a String containing a welcome message.
+   */
+  @GetMapping
+  public String home() {
+    return "Wallet's API Gateway is operational! 🚀";
+  }
 }
---
+++ api-gateway/src/main/java/com/fact/wallet/api_gateway/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the API Gateway for the Wallet microservice. It is responsible for routing
+ * requests to the appropriate microservices and handling cross-cutting concerns such as
+ * authentication, logging, and rate limiting. The API Gateway is implemented using Spring Cloud
+ * Gateway.
+ */
+package com.fact.wallet.api_gateway;
