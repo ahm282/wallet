98ec91fdb836fdda263e75098e785c17136ac44b
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-20 10:31:42 +0200

    refactor(frontend): add doc title change - add auth userId step 1

--- frontend/src/pages/Accounts/AccountsPage.tsx
+++ frontend/src/pages/Accounts/AccountsPage.tsx
@@ -1,42 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { Account } from "@/types/accounts.types";
 import NoAccounts from "@/components/accounts/NoAccounts";
 import AccountsDataExists from "@/components/accounts/AccountsDataExists";
 
 export const AccountsPage = () => {
-  // Sample data
-  const sampleAccounts = [
-    {
-      id: 1,
-      name: "Checking",
-      institution: "KBC",
-      balance: 1000,
-      currency: "EUR",
-    },
-    {
-      id: 2,
-      name: "Savings",
-      institution: "Argenta",
-      balance: 5000,
-      currency: "USD",
-    },
-    {
-      id: 3,
-      name: "Credit Card",
-      institution: "Chase",
-      balance: -500,
-      currency: "USD",
-    },
-  ];
+    // Sample data
+    const sampleAccounts = [
+        {
+            id: 1,
+            name: "Checking",
+            institution: "KBC",
+            balance: 1000,
+            currency: "EUR",
+        },
+        {
+            id: 2,
+            name: "Savings",
+            institution: "Argenta",
+            balance: 5000,
+            currency: "USD",
+        },
+        {
+            id: 3,
+            name: "Credit Card",
+            institution: "Chase",
+            balance: -500,
+            currency: "USD",
+        },
+    ];
 
-  const [accounts, setAccounts] = useState<Account[]>(sampleAccounts);
-  const hasAccountData = accounts.length > 0;
+    const [accounts, setAccounts] = useState<Account[]>(sampleAccounts);
+    const hasAccountData = accounts.length > 0;
 
-  return hasAccountData ? (
-    <AccountsDataExists accounts={accounts} setAccounts={setAccounts} />
-  ) : (
-    <NoAccounts accounts={accounts} setAccounts={setAccounts} />
-  );
+    /*
+     * Sets the title of the page to "Accounts | Wallet" when the component mounts
+     */
+    useEffect(() => {
+        document.title = "Accounts | Wallet";
+    }, []);
+
+    return hasAccountData ? (
+        <AccountsDataExists
+            accounts={accounts}
+            setAccounts={setAccounts}
+        />
+    ) : (
+        <NoAccounts
+            accounts={accounts}
+            setAccounts={setAccounts}
+        />
+    );
 };
 
 export default AccountsPage;
--- frontend/src/pages/Transactions/TransactionsPage.tsx
+++ frontend/src/pages/Transactions/TransactionsPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { PlusCircle, List } from "lucide-react";
@@ -13,94 +13,100 @@ import { Category } from "@/types/transactions.types";
 import type { Transaction, TransactionForm } from "@/types/transactions.types";
 
 export const TransactionsPage = () => {
-  const { transactions, addTransaction, editTransaction, deleteTransaction } =
-    useTransactions();
-  const {
-    searchTerm,
-    setSearchTerm,
-    categoryFilter,
-    setCategoryFilter,
-    dateRange,
-    setDateRange,
-    filteredTransactions,
-  } = useTransactionFilters(transactions);
+    const { transactions, addTransaction, editTransaction, deleteTransaction } = useTransactions();
+    const {
+        searchTerm,
+        setSearchTerm,
+        categoryFilter,
+        setCategoryFilter,
+        dateRange,
+        setDateRange,
+        filteredTransactions,
+    } = useTransactionFilters(transactions);
 
-  const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
-  const [editingTransaction, setEditingTransaction] =
-    useState<Transaction | null>(null);
-  const [newTransaction, setNewTransaction] = useState<TransactionForm>({
-    date: undefined,
-    description: "",
-    amount: 0,
-    category: null,
-  });
+    const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+    const [editingTransaction, setEditingTransaction] = useState<Transaction | null>(null);
+    const [newTransaction, setNewTransaction] = useState<TransactionForm>({
+        date: undefined,
+        description: "",
+        amount: 0,
+        category: null,
+    });
 
-  const handleEditClick = (transaction: Transaction) => {
-    setEditingTransaction(transaction);
-    setIsEditDialogOpen(true);
-  };
+    /*
+     * Sets the title of the page to "Transactions | Wallet" when the component mounts
+     */
+    useEffect(() => {
+        document.title = "Transactions | Wallet";
+    }, []);
 
-  return (
-    <div className="w-11/12 md:w-10/12 lg:max-w-6xl 2xl:max-w-7xl my-8 mx-auto flex flex-col space-y-5">
-      <Card className="lg:min-h-[400px]">
-        <CardHeader>
-          <CardTitle className="flex items-center font-primary text-2xl font-bold">
-            <List className="h-6 w-6 me-3" />
-            Transactions
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="lg:flex lg:justify-between lg:items-center lg:px-1">
-            <TransactionsFilters
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-              categoryFilter={categoryFilter}
-              setCategoryFilter={setCategoryFilter}
-              dateRange={dateRange}
-              setDateRange={setDateRange}
+    const handleEditClick = (transaction: Transaction) => {
+        setEditingTransaction(transaction);
+        setIsEditDialogOpen(true);
+    };
+
+    return (
+        <div className='w-11/12 md:w-10/12 lg:max-w-6xl 2xl:max-w-7xl my-8 mx-auto flex flex-col space-y-5'>
+            <Card className='lg:min-h-[400px]'>
+                <CardHeader>
+                    <CardTitle className='flex items-center font-primary text-2xl font-bold'>
+                        <List className='h-6 w-6 me-3' />
+                        Transactions
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className='lg:flex lg:justify-between lg:items-center lg:px-1'>
+                        <TransactionsFilters
+                            searchTerm={searchTerm}
+                            setSearchTerm={setSearchTerm}
+                            categoryFilter={categoryFilter}
+                            setCategoryFilter={setCategoryFilter}
+                            dateRange={dateRange}
+                            setDateRange={setDateRange}
+                        />
+                        <div className='flex flex-col'>
+                            <Label
+                                htmlFor='add-transaction'
+                                className='invisible'>
+                                Add Transaction
+                            </Label>
+                            <Button
+                                id='add-transaction'
+                                className='w-full lg:max-w-40'
+                                onClick={() => setIsAddDialogOpen(true)}>
+                                <PlusCircle className='mr-2 h-4 w-4' />
+                                Add Transaction
+                            </Button>
+                        </div>
+                    </div>
+                    <TransactionsContent
+                        transactions={transactions}
+                        filteredTransactions={filteredTransactions}
+                        onEdit={handleEditClick}
+                        onDelete={deleteTransaction}
+                    />
+                </CardContent>
+            </Card>
+
+            <AddTransactionDialog
+                isOpen={isAddDialogOpen}
+                setIsOpen={setIsAddDialogOpen}
+                onAdd={addTransaction}
+                newTransaction={newTransaction}
+                setNewTransaction={setNewTransaction}
+                categories={Object.values(Category)}
             />
-            <div className="flex flex-col">
-              <Label htmlFor="add-transaction" className="invisible">
-                Add Transaction
-              </Label>
-              <Button
-                id="add-transaction"
-                className="w-full lg:max-w-40"
-                onClick={() => setIsAddDialogOpen(true)}
-              >
-                <PlusCircle className="mr-2 h-4 w-4" />
-                Add Transaction
-              </Button>
-            </div>
-          </div>
-          <TransactionsContent
-            transactions={transactions}
-            filteredTransactions={filteredTransactions}
-            onEdit={handleEditClick}
-            onDelete={deleteTransaction}
-          />
-        </CardContent>
-      </Card>
 
-      <AddTransactionDialog
-        isOpen={isAddDialogOpen}
-        setIsOpen={setIsAddDialogOpen}
-        onAdd={addTransaction}
-        newTransaction={newTransaction}
-        setNewTransaction={setNewTransaction}
-        categories={Object.values(Category)}
-      />
-
-      <EditTransactionDialog
-        isOpen={isEditDialogOpen}
-        setIsOpen={setIsEditDialogOpen}
-        transaction={editingTransaction}
-        onSave={editTransaction}
-        categories={Object.values(Category)}
-      />
-    </div>
-  );
+            <EditTransactionDialog
+                isOpen={isEditDialogOpen}
+                setIsOpen={setIsEditDialogOpen}
+                transaction={editingTransaction}
+                onSave={editTransaction}
+                categories={Object.values(Category)}
+            />
+        </div>
+    );
 };
 
 export default TransactionsPage;
