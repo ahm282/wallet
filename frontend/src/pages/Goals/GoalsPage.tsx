72967fffa776705edb0fa3a58e8d2c992d06aa58
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-21 00:16:53 +0100

    refactor(frontend): add preliminary api calls for goals

--- frontend/src/pages/Goals/GoalsPage.tsx
+++ frontend/src/pages/Goals/GoalsPage.tsx
@@ -7,35 +7,35 @@ export const GoalsPage = () => {
     // Sample data
     const sampleGoals = [
         {
-            id: 1,
+            id: "1",
             name: "Emergency fund",
             target: 1000,
             current: 500,
             targetDate: "2022-12-31",
         },
         {
-            id: 2,
+            id: "2",
             name: "Vacation",
             target: 2000,
             current: 1000,
             targetDate: "2023-06-30",
         },
         {
-            id: 3,
+            id: "3",
             name: "New car",
             target: 15000,
             current: 5000,
             targetDate: "2024-12-31",
         },
         {
-            id: 4,
+            id: "4",
             name: "New house",
             target: 200000,
             current: 20000,
             targetDate: "2026-12-31",
         },
         {
-            id: 5,
+            id: "5",
             name: "New computer",
             target: 2000,
             current: 200,
