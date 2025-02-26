364ef7218bff07263853ffc442dc9fa08150c2ce
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-26 07:11:55 +0100

    refactor(frontend): table css changes - accounts types refactor WIP

--- frontend/src/types/accounts.types.ts
+++ frontend/src/types/accounts.types.ts
@@ -1,44 +1,57 @@
 // Accounts definitions
 export interface Account {
-  id: number;
-  name: string;
-  institution: string;
-  balance: number;
-  currency: string;
+    id: string;
+    name: string;
+    institution: string;
+    balance: number;
+    currency: string;
 }
 
 export interface NoAccountsProps {
-  accounts: Account[];
-  setAccounts: React.Dispatch<React.SetStateAction<Account[]>>;
+    accounts: Account[];
+    setAccounts: React.Dispatch<React.SetStateAction<Account[]>> | (() => void);
 }
 
 export interface AccountsDataExistsProps {
-  accounts: Account[];
-  setAccounts: React.Dispatch<React.SetStateAction<Account[]>>;
+    accounts: Account[];
+    setAccounts: React.Dispatch<React.SetStateAction<Account[]>> | (() => void);
 }
 
 export interface AddAccountDialogProps {
-  accounts: Account[];
-  setAccounts: React.Dispatch<React.SetStateAction<Account[]>>;
-  children?: React.ReactNode;
+    accounts: Account[];
+    setAccounts: React.Dispatch<React.SetStateAction<Account[]>> | (() => void);
+    children?: React.ReactNode;
 }
 
 export interface EditAccountDialogProps {
-  account: Account | null;
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  onSave: (account: Account) => void;
+    account: Account | null;
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    onSave: (account: Account) => void;
 }
 
 export type AccountForm = Omit<Account, "id">;
 
 export interface AccountFormErrors {
-  name: string;
-  institution: string;
-  balance: string;
-  currency: string;
+    name: string;
+    institution: string;
+    balance: string;
+    currency: string;
 }
 
 export interface EditedAccountForm extends AccountForm {
-  id: number;
+    id: number;
+}
+
+export interface AccountsTableProps {
+    accounts: Account[];
+    handleEdit: (account: Account) => void;
+    handleDelete: (id: string) => void;
+}
+
+export interface AccountErrors {
+    name: string;
+    balance: string;
+    institution: string;
+    currency: string;
 }
