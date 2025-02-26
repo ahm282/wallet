30633fb859f2ca09ed9252fc4acf97155d83f1fa
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-02-26 05:39:40 +0100

    refactor(finance-service): add goal CRUD

--- finance-service/src/main.ts
+++ finance-service/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestFastifyApplication, FastifyAdapter } from "@nestjs/platform-fastify";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
         abortOnError: false,
     });
-    await app.listen(process.env.PORT ?? 3000);
+
+    app.setGlobalPrefix("/api/finance");
+    await app.listen(process.env.NESTJS_PORT ?? 3000, "0.0.0.0");
 }
+
 bootstrap();
