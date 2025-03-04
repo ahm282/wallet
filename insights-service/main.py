0eaa52248ef309dd5e9f93e121cfc6459df11672
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-03-04 12:30:15 +0200

    chore(insights-service): add dockerfile for the service

--- insights-service/main.py
+++ insights-service/main.py
@@ -21,5 +21,8 @@ app.include_router(
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = os.getenv("INSIGHTS_PORT", 8083)
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
